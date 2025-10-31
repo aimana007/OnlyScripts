49504015c14653f78e23c886880452b27daca14c
AimanA <1709028+aimana007@users.noreply.github.com>
2025-10-31 12:39:41 -0400

    Refactor AlterCaps.sh to accept arguments

--- bash/AlterCaps.sh
+++ bash/AlterCaps.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-# Read from stdin
-while IFS= read -r line; do
-    result=""
-    letter_count=0
+# If arguments provided, use them; otherwise read from stdin
+if [ $# -gt 0 ]; then
+    input="$*"
+else
+    input=$(cat)
+fi
+result=""
+letter_count=0
+for (( i=0; i<${#input}; i++ )); do
+    char="${input:$i:1}"
     
-    for (( i=0; i<${#line}; i++ )); do
-        char="${line:$i:1}"
-        
-        if [[ $char =~ [a-zA-Z] ]]; then
-            if (( letter_count % 2 == 0 )); then
-                result+="${char^^}"  # Uppercase
-            else
-                result+="${char,,}"  # Lowercase
-            fi
-            ((letter_count++))
+    if [[ $char =~ [a-zA-Z] ]]; then
+        if (( letter_count % 2 == 0 )); then
+            result+="${char^^}"
         else
-            # Keep non-letter characters as-is
-            result+="$char"
+            result+="${char,,}"
         fi
-    done
-    
-    echo "$result"
+        ((letter_count++))
+    else
+        result+="$char"
+    fi
 done
+echo "$result"
