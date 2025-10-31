c470b44bb7bb932a0fd3a7c3faebc6392f26c074
AimanA <1709028+aimana007@users.noreply.github.com>
2025-10-31 12:41:25 -0400

    Refactor text processing in AlterCaps.ps1

--- PowerShell/AlterCaps.ps1
+++ PowerShell/AlterCaps.ps1
@@ -2,17 +2,13 @@ param(
     [Parameter(ValueFromPipeline=$true, ValueFromRemainingArguments=$true)]
     [string[]]$InputText
 )
-
 process {
     # Handle both pipeline input and command-line arguments
-    $text = if ($InputText) { $InputText -join ' ' } else { $_ }
-    
+    $text = if ($InputText) { $InputText -join ' ' } else { $_ }  
     $result = ""
     $letterCount = 0
-    
     for ($i = 0; $i -lt $text.Length; $i++) {
         $char = $text[$i]
-        
         if ($char -match '[a-zA-Z]') {
             if ($letterCount % 2 -eq 0) {
                 $result += $char.ToString().ToUpper()
@@ -24,6 +20,5 @@ process {
             $result += $char
         }
     }
-    
     $result
 }
